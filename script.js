c1f6a5e4a79828b017e5ac71b7df0d25dc234df2
aspou2413 <76095589+aspou2413@users.noreply.github.com>
2024-01-15 00:23:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-let TICK = 10;
-let deg = 0;
-let image = document.getElementById("rotated");
-let mainLoop = function(){  
-  image.style.transform = "rotate3d(1, 1, 1, "+ deg +"deg)";  
-  deg += 2;  
-    setTimeout(mainLoop, TICK);
-  }
-window.onload = function(){
-  mainLoop();
-}
+
